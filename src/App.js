7f410f4852989b51a21948904c050c66b8e2f86e
valwebd <77312053+valwebd@users.noreply.github.com>
2021-07-10 14:36:39 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { /*Fragment,*/ Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar.js';
+import Users from './components/users/Users.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    // const name = "Val Pal";
+
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
